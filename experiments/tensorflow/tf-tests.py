86b9cea2ef1eeda10d620505f4e01c054dda4d17
floghos <caleuche94@gmail.com>
2021-07-08 16:03:26 -0400

    starting tensorflow tests

---
+++ experiments/tensorflow/tf-tests.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  8 13:30:53 2021
+
+@author: caleu
+"""
+
+import tensorflow as tf
+
+print("this is the current tensorflow version: ")
+print(str(tf.version) + '\n')
+
