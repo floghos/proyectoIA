bd1ffbd8e5272683d9ed594abc239a609a470046
floghos <caleuche94@gmail.com>
2021-07-31 21:15:03 -0400

    Very important fixes for the API, now it's working consistently

--- prototipo_final/API/sg_api.py
+++ prototipo_final/API/sg_api.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from time import sleep
 
-from .inputpreprocess import observation, Map, lives, enemy_lives, enemy_lives_2
+from .inputpreprocess import observation, Map, lives, enemy_lives, enemy_lives_2, renderObs
 from .outputcommands import press, release, tap
 
 # Use the 2 below instead of the 2 above when testing the module by itself
@@ -28,9 +28,11 @@ def setup() -> Map:
     # selecting map and difficulty
     tap('swing')
     for i in range(5):
+        sleep(0.05)
         tap('down')
+    #sleep(0.1)
     tap('swing')
-
+    sleep(1.2)
     empty = Map(0, )
     screenshot = observation(empty, raw=True)
     map = Map(0, screenshot)
@@ -43,22 +45,15 @@ def restart() -> None:
     '''
     Abre el menu de pausa para reiniciar la partida en caso de que el jugador o el enemigo se le acaben las vidass
     '''
-    ##
-    # Como minimo debemos:
-    #   -presionar start
-    #   -seleccionar restart
-    #   -esperar un momento (~1 seg) antes de crear map (para evitar que la animacion del menu afecten la mascara)
-    ##
-    WAIT = 0.1
+    releaseAllKeys()
+
+    WAIT = 0.05
     tap('menu')
     sleep(WAIT)
     tap('down')
     sleep(WAIT)
     tap('swing')
-    # sleep(1)
-    # tap('menu')
-    # sleep(1.5)
-    # tap('menu')
+    sleep(0.2)
     print("sleep time's over")
  
 
@@ -82,22 +77,23 @@ def reset(map: Map):
     return initial_state, start
 
 
-DELAY = 0.1
+DELAY = 0.0
+
+ACTIONS_DICT={0:'left', 1:'left', 2:'right', 3:'right', 4:'up', 5:'up', 6: 'down', 7:'down', 8:'jump', 9:'jump', 10:'swing'}
 
 def step(action, map: Map):
     done = False
     restart = False  # Flag that signals whenever either the player's or the enemy's lives have reached 0, hence requiring to restart
     
     # assign to every number of action a key for execute the code. I think this can be automatized.
-    dict_actions={0:'left', 1:'left', 2:'right', 3:'right', 4:'up', 5:'up', 6: 'down', 7:'down', 8:'jump', 9:'jump', 10:'swing'}
 
     # split between 0 n' 1 if action is odd or even, even is release, odd is press
     if action == 10:
-        tap(dict_actions[action])
+        tap(ACTIONS_DICT[action])
     elif action % 2 == 1:
-        press(dict_actions[action])
+        press(ACTIONS_DICT[action])
     elif action % 2 == 0:
-        release(dict_actions[action])
+        release(ACTIONS_DICT[action])
     
     sleep(DELAY)
     new_state, raw_, _ = observation(map)
@@ -106,7 +102,7 @@ def step(action, map: Map):
     current_e_lives = enemy_lives_2(raw_)
 
     #print(f'{current_p_lives = }, {current_e_lives = }')
-
+    # Checking if any player has lost any lives
     p_lives_diff = current_p_lives - map.p_lives
     e_lives_diff = current_e_lives - map.e_lives
 
@@ -115,14 +111,11 @@ def step(action, map: Map):
     if e_lives_diff == -1:
         reward += 1
         done = True
-
     if p_lives_diff == -1:
         reward -= 1
         done = True
-
     if e_lives_diff - p_lives_diff == 0:
         reward -= 0.01
-
     if (current_p_lives == 0) or (current_e_lives == 0):
         restart = True
 
@@ -130,25 +123,14 @@ def step(action, map: Map):
     map.e_lives = current_e_lives
     return new_state, reward, done, restart
 
+def render(obs):
+    renderObs(obs)
+
+def releaseAllKeys():
+    for action in range(5):
+        release(ACTIONS_DICT[action*2])
+
 if __name__ == '__main__':
     sleep(2)
     restart()
-    # for i in range(5):
-    #     tap('swing')
-    #     sleep(0.3)
 
-    # press('right')
-    # sleep(1)
-    # release('right')
-
-    # press('swing')
-    # sleep(0.1)
-    # release('swing')
-
-    # press('left')
-    # sleep(1)
-    # release('left')
-
-    # press('right')
-    # sleep(0.1)
-    # release('right')
